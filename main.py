8588bc6db9c8f2bd69b30c24d3429560b52c598d
Amal Abdirahman <150919618+AmalAbdirahman@users.noreply.github.com>
2026-05-08 15:08:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -383,7 +383,7 @@ with tab2:
         # Target: The position of the driver 3 laps into the future
         y = df['Position'].shift(-3).fillna(df['Position'].max())
         
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
         
         model = GradientBoostingRegressor(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
